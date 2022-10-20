4e3e85fe4213cb2c09d75a60bdbd61ff9ab5dca6
ValtteriLento <valtteri.lento@gmail.com>
2022-10-20 16:53:31 +0000

    Update 4 files

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -178,10 +178,10 @@ function initializeCode() {
     _createClass(PlayGame, [{
       key: "preload",
       value: function preload() {
-        this.load.image("sky", "assets/sky.png");
-        this.load.image("ground", "assets/platform.png");
-        this.load.image("star", "assets/star.png");
-        this.load.spritesheet("dude", "assets/dude.png", {
+        this.load.image("sky", "./assets/sky.png");
+        this.load.image("ground", "./assets/platform.png");
+        this.load.image("star", "./assets/star.png");
+        this.load.spritesheet("dude", "./assets/dude.png", {
           frameWidth: 32,
           frameHeight: 48
         });
--- src/index.js
+++ src/index.js
@@ -47,10 +47,10 @@ function initializeCode() {
     }
 
     preload() {
-      this.load.image("sky", "assets/sky.png");
-      this.load.image("ground", "assets/platform.png");
-      this.load.image("star", "assets/star.png");
-      this.load.spritesheet("dude", "assets/dude.png", {
+      this.load.image("sky", "./assets/sky.png");
+      this.load.image("ground", "./assets/platform.png");
+      this.load.image("star", "./assets/star.png");
+      this.load.spritesheet("dude", "./assets/dude.png", {
         frameWidth: 32,
         frameHeight: 48,
       });
